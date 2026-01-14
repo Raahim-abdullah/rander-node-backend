1ce9ed557e5ff1d0687f1574364c6ba0866e830d
asmras7 <raahimabdullah441@gmail.com>
2026-01-14 23:36:50 +0500

    added express static

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const cros = require('cors')
 const app = express()
 
 app.use(express.json())
+app.use(express.static('dist'))
 const requestLogger = (request, response, next) => {
     console.log('Method:', request.method)
     console.log('Path:  ', request.path)
