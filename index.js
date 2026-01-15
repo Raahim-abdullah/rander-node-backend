03f732004ad8ef2f8c3ee771e000e43e6a4d88b6
asmras7 <raahimabdullah441@gmail.com>
2026-01-15 18:50:27 +0500

    Add mongoose integration and error handling for notes API

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
+require("dotenv").config()
 const express = require('express')
+const Note = require("./models/note.js")
 const cros = require('cors')
 
 const app = express()
@@ -14,6 +16,21 @@ const requestLogger = (request, response, next) => {
 }
 app.use(cros())
 app.use(requestLogger)
+
+const errorHandler = (error, req, res, next) => {
+    console.error(error.message)
+
+    if (error.name === 'CastError') {
+        return res.status(400).send({ error: 'malformatted id' })
+    } else if (error.name === "ValidationError") {
+        return res.status(400).json({ error: error.message })
+    }
+
+    next(error)
+}
+const unknownEndpoint = (req, res) => {
+    res.status(404).send({ error: 'unknown endpoint' })
+}
 
 let notes = [
     {
@@ -33,38 +50,37 @@ let notes = [
     }
 ]
 
-const generateId = () => {
-    const maxId = notes.length > 0 ? Math.max(...notes.map(n => Number(n.id))) : 0
-    return String(maxId + 1)
-
-}
-
-app.get("/", (request, response) => {
-    response.send("<h1>Hello, world!</h1>")
+app.get("/", (req, res) => {
+    res.send("<h1>Hello, world!</h1>")
 })
 
 app.get("/api/notes", (req, res) => {
-    res.json(notes)
+    Note.find({}).then(notes => {
+        res.json(notes)
+    })
 })
 
-app.get("/api/notes/:id", (request, response) => {
-    console.log("yes i ran.")
-    const id = request.params.id
-    const note = notes.find(n => n.id === id)
-    if (note) {
-        return response.json(note)
-    }
-    return response.status(404).end()
+app.get("/api/notes/:id", (req, res, next) => {
+    Note.findById(req.params.id)
+        .then(note => {
+            if (note) {
+                res.json(note)
+            } else {
+                res.status(404).end()
+            }
+        })
+        .catch(error => next(error))
 })
 
-app.delete("/api/notes/:id", (req, res) => {
-    const id = req.params.id
-    notes = notes.filter(note => note.id !== id)
-
-    return res.status(204).end()
+app.delete("/api/notes/:id", (req, res, next) => {
+    Note.findByIdAndDelete(req.params.id)
+        .then(result => {
+            res.status(204).json(result)
+        })
+        .catch(error => next(error))
 })
 
-app.post("/api/notes", (req, res) => {
+app.post("/api/notes", (req, res, next) => {
     const body = req.body
 
     if (!body.content) {
@@ -72,21 +88,41 @@ app.post("/api/notes", (req, res) => {
             error: "content missing"
         })
     }
-    const note = {
+    const note = new Note({
         content: body.content,
         important: body.important || false,
-        id: generateId(),
-    }
+    })
 
-    notes = notes.concat(note)
-    return res.json(note)
+    note.save()
+        .then(savedNote => {
+            return res.json(savedNote)
+        })
+        .catch(error => next(error))
 })
 
-const unknownEndpoint = (request, response) => {
-    response.status(404).send({ error: 'unknown endpoint' })
-}
+app.put("/api/notes/:id", (req, res, next) => {
+    const { content, important } = req.body
+
+    Note.findById(req.params.id)
+        .then(note => {
+            if (!note) {
+                return res.status(404).end()
+            }
+
+            note.content = content
+            note.important = important
+
+            return note.save()
+                .then(updateNote => {
+                    res.json(updateNote)
+                })
+        })
+        .catch(error => next(error))
+})
+
 
 app.use(unknownEndpoint)
+app.use(errorHandler)
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
---
+++ mango.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose')
+
+if (process.argv.length < 3) {
+    console.log('give password as argument')
+    process.exit(1)
+}
+
+const password = process.argv[2]
+
+const url = `mongodb+srv://raahimabdullah142_db_user:${password}@cluster0.9rozfor.mongodb.net/noteApp?appName=Cluster0`
+
+mongoose.set('strictQuery', false)
+
+mongoose.connect(url, { family: 4 })
+
+const noteSchema = new mongoose.Schema({
+    content: String,
+    important: Boolean,
+})
+
+const Note = mongoose.model('Note', noteSchema)
+
+// const note = new Note({
+//     content: 'mangoose work like they say',
+//     important: true,
+// })
+Note.find({}).then(result => {
+    result.forEach(note => {
+        console.log(note)
+    })
+    mongoose.connection.close()
+})
+// note.save().then(result => {
+//     console.log('note saved!')
+//     mongoose.connection.close()
+// })
